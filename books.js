2ccfe626db39d777ed69886d63b06475aa077f2a
Ryan Mello <74434148+ryanmello@users.noreply.github.com>
2022-09-15 18:50:00 -0700

    site finished

--- books.js
+++ books.js
@@ -4,17 +4,18 @@ function renderBooks(filter){
   const books = getBooks();
 
   if(filter === 'LOW_TO_HIGH'){
-    books.sort((a, b) => a.originalPrice - b.originalPrice);
+    books.sort((a, b) => (a.salePrice || a.originalPrice) - (b.salePrice || b.originalPrice));
   }
   else if(filter === "HIGH_TO_LOW"){
-    books.sort((a, b) => b.originalPrice - a.originalPrice);
+    books.sort((a, b) => (b.salePrice || b.originalPrice) - (a.salePrice || a.originalPrice));
   }
   else if(filter === "RATING"){
     books.sort((a, b) => b.rating - a.rating);
   }
 
-  const booksHtml = books.map((book) => {
-    return `<div class="book">
+  const booksHtml = books
+    .map((book) => {
+      return `<div class="book">
     <figure class="book__img--wrapper">
       <img class="book__img" src="${book.url}" alt="">
     </figure>
@@ -25,11 +26,22 @@ function renderBooks(filter){
       ${ratingsHTML(book.rating)}
     </div>
     <div class="book__price">
-      <span class=>$${book.originalPrice.toFixed(2)}</span>
+      ${priceHTML(book.originalPrice, book.salePrice)}
     </div>
-  </div>`
-  }).join('');
+  </div>`;
+    })
+    .join("");
+
   booksWrapper.innerHTML = booksHtml;
+}
+
+function priceHTML(originalPrice, salePrice) {
+  if (!salePrice) {
+    return `$${originalPrice.toFixed(2)}`;
+  }
+  return `<span class="book__price--normal">$${originalPrice.toFixed(
+    2
+  )}</span>$${salePrice.toFixed(2)}`;
 }
 
 function ratingsHTML(rating){
